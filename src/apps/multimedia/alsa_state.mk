88fdc9b40bc9e25b50bbe2508729741d90fa7c95
shangavelan <shangavelan.m@adlinktech.com>
2025-01-24 15:54:41 +0530

    alsa_state mk changes and mkimage changes

--- src/apps/multimedia/alsa_state.mk
+++ src/apps/multimedia/alsa_state.mk
@@ -11,5 +11,9 @@ alsa_state:
 	 $(call fbprint_b,"alsa_state") && \
 	 install -d $(DESTDIR)/var/lib/alsa && \
 	 install -m 0644 $(FBDIR)/patch/alsa_state/asound.state $(DESTDIR)/var/lib/alsa && \
+	 if [ "$(MACHINE)" = "sp2imx8mp" ]; then \
+	     $(call fbprint_d,"alsa_state_sp2") && \
+	     install -m 0644 $(FBDIR)/patch/alsa_state/sp2_asound.state $(DESTDIR)/var/lib/alsa/asound.state; \
+	 fi && \
 	 install -m 0644 $(FBDIR)/patch/alsa_state/asound.conf $(DESTDIR)/etc && \
 	 $(call fbprint_d,"alsa_state")
